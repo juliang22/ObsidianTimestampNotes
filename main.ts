5820d1bea8ed3e12b71aabfe24124766a14a0d1f
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2022-05-16 14:25:04 -0700

    main.js added

--- main.ts
+++ main.ts
@@ -121,12 +121,6 @@ export default class YoutubeTimestampPlugin extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new YoutubeTimestampPluginSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
 	}
 
 	async onunload() {
