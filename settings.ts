1598c5e2f1b08e8f24bd9ebb8883aee52ebd7499
zuluwi <111116092+zuluwi@users.noreply.github.com>
2023-06-30 00:39:13 +0300

    update obsidian and add color picker for button settings

--- settings.ts
+++ settings.ts
@@ -17,17 +17,15 @@ export interface TimestampPluginSettings {
 export const DEFAULT_SETTINGS: TimestampPluginSettings = {
 	noteTitle: "",
 	urlStartTimeMap: new Map<string, number>(),
-	urlColor: 'blue',
-	timestampColor: 'green',
-	urlTextColor: 'white',
-	timestampTextColor: 'white',
+	urlColor: '#0000FF',
+	timestampColor: '#008000',
+	urlTextColor: '#FFFFFF',
+	timestampTextColor: '#FFFFFF',
 	forwardSeek: '10',
 	backwardsSeek: '10',
 	port: 12345,
 	startAtLastPosition: true
 }
-
-const COLORS = { 'blue': 'blue', 'red': 'red', 'green': 'green', 'yellow': 'yellow', 'orange': 'orange', 'purple': 'purple', 'pink': 'pink', 'grey': 'grey', 'black': 'black', 'white': 'white' };
 
 const TIMES = { '5': '5', '10': '10', '15': '15', '20': '20', '25': '25', '30': '30', '35': '35', '40': '40', '45': '45', '50': '50', '55': '55', '60': '60', '65': '65', '70': '70', '75': '75', '80': '80', '85': '85', '90': '90', '95': '95', '100': '100', '105': '105', '110': '110', '115': '115', '120': '120' }
 
@@ -58,41 +56,52 @@ export class TimestampPluginSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 
+		containerEl.createEl("p")
+		const urlButton = containerEl.createEl("button");
+		urlButton.innerText = "timestamp url";
+		urlButton.style.backgroundColor = this.plugin.settings.urlColor;
+		urlButton.style.color = this.plugin.settings.urlTextColor;
+		
 		// Customize  url button color
 		new Setting(containerEl)
 			.setName('URL Button Color')
 			.setDesc('Pick a color for the url button.')
-			.addDropdown(dropdown => dropdown
-				.addOptions(COLORS)
+			.addColorPicker(color => color
 				.setValue(this.plugin.settings.urlColor)
 				.onChange(async (value) => {
 					this.plugin.settings.urlColor = value;
+					urlButton.style.backgroundColor = this.plugin.settings.urlColor;
 					await this.plugin.saveSettings();
 				}
 				));
-
 		// Customize url text color
 		new Setting(containerEl)
 			.setName('URL Text Color')
 			.setDesc('Pick a color for the URL text button.')
-			.addDropdown(dropdown => dropdown
-				.addOptions(COLORS)
+			.addColorPicker(color => color
 				.setValue(this.plugin.settings.urlTextColor)
 				.onChange(async (value) => {
 					this.plugin.settings.urlTextColor = value;
+					urlButton.style.color = this.plugin.settings.urlTextColor;
 					await this.plugin.saveSettings();
 				}
 				));
 
+		containerEl.createEl("p")
+		const timestampButton = containerEl.createEl("button");
+		timestampButton.innerText = "01:23";
+		timestampButton.style.backgroundColor = this.plugin.settings.timestampColor;
+		timestampButton.style.color = this.plugin.settings.timestampTextColor;
+		
 		// Customize timestamp button color
 		new Setting(containerEl)
 			.setName('Timestamp Button Color')
 			.setDesc('Pick a color for the timestamp button.')
-			.addDropdown(dropdown => dropdown
-				.addOptions(COLORS)
+			.addColorPicker(color => color
 				.setValue(this.plugin.settings.timestampColor)
 				.onChange(async (value) => {
 					this.plugin.settings.timestampColor = value;
+					timestampButton.style.backgroundColor = this.plugin.settings.timestampColor;
 					await this.plugin.saveSettings();
 				}
 				));
@@ -101,15 +110,17 @@ export class TimestampPluginSettingTab extends PluginSettingTab {
 		new Setting(containerEl)
 			.setName('Timestamp Text Color')
 			.setDesc('Pick a color for the timestamp text.')
-			.addDropdown(dropdown => dropdown
-				.addOptions(COLORS)
+			.addColorPicker(color => color
 				.setValue(this.plugin.settings.timestampTextColor)
 				.onChange(async (value) => {
-					this.plugin.settings.timestampTextColor = value;
+					this.plugin.settings.timestampColor = value;
+					timestampButton.style.color = this.plugin.settings.timestampTextColor;
 					await this.plugin.saveSettings();
 				}
 				));
 
+		containerEl.createEl("p")
+		
 		// Customize forward seek time
 		new Setting(containerEl)
 			.setName('Foward time seek')
